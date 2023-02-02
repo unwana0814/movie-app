7a493f26ef86eef9c53df4bf2b8d9277780f25d0
Unwana <brettygeorge3@gmail.com>
2023-02-02 22:19:30 +0000

    Add try catch

--- src/App.jsx
+++ src/App.jsx
@@ -17,14 +17,17 @@ const movie1 = {
 
 const App = () => {
 
-  const [movies, setMovies] = useState();
+  const [movies, setMovies] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
 
   const searchMovies = async (title) => {
-    const response = await fetch(`${API_URL}&s=${title}`);
-    const data = await response.json();
-
-    setMovies(data.Search);
+    try {
+       const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+        setMovies(data.Search);
+    } catch (error) {
+      console.log(error)
+    }
   }
   useEffect(() => {
     searchMovies(searchTerm);
